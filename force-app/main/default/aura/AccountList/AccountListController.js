dbc301eb6682c7ed78cbd44a9845c0b8bd1207fb
e-deslan <eduardo.deslandes@gmail.com>
2018-08-16 17:24:58 -0300

    account map display

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -2,7 +2,10 @@
     doInit : function(component, event) {
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
-          component.set("v.accounts", a.getReturnValue()); 
+            component.set("v.accounts", a.getReturnValue());
+            var ev = $A.get("e.c:AccountLoaded");
+            ev.setParams({"accounts": a.getReturnValue()});
+            ev.fire();
         });
         $A.enqueueAction(action);
     }
